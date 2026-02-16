18fe263d92fe357a45c3de854cb6b0dffdc3ace5
yoyo <lukas.de1keizer@gmail.com>
2026-02-16 23:33:05 +0000

    ?

--- frontend_next/app_self/src/app/to/app_conf.tsx
+++ frontend_next/app_self/src/app/to/app_conf.tsx
@@ -2,7 +2,7 @@
 console.log("API HOST:", process.env.NEXT_PUBLIC_API_BASE_FETCH);
 
 const app_title = 'FsQue'
-const default_app_url = '/fsque/to/lobby'
+const default_app_url = '/to/lobby'
 
 const CHUNK_SIZE = 10 * 1024  * 1024
 

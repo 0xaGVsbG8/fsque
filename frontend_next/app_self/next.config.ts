faf70deba330470133b7c88642598727bf33135b
Lukas de Keizer <lukas.de1keizer@gmail.com>
2026-02-17 02:50:12 +0100

    Update next.config.ts

--- frontend_next/app_self/next.config.ts
+++ frontend_next/app_self/next.config.ts
@@ -5,26 +5,9 @@ import path from "path";
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
-// module.exports = {
-//   env: {
-//     NEXT_PUBLIC_API_PORT: process.env.API_PORT,
-//     NEXT_PUBLIC_API_BASE_URL: process.env.API_BASE_URL,
-//     NEXT_PUBLIC_USE_SSL: process.env.USE_SSL,
-//     basePath: '/fsque',
-//   }
-// }
 
 const nextConfig: NextConfig = {
 
-
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/fsque/:path*',
-  //       destination: '/:path*',
-  //     },
-  //   ]
-  // },
 
   /* config options here */
   basePath: '/fsque',
